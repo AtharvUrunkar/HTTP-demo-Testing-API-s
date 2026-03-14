f653922c83543e0c814245d65263de65594ed031
Atharv <atharvurunkar@gmail.com>
2026-03-14 23:17:54 +0530

    booking creation in BookingController

--- demo/src/main/java/com/example/demo/controllers/BookingController.java
+++ demo/src/main/java/com/example/demo/controllers/BookingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class BookingController {
 
 	private List<Booking> bookings = new ArrayList<>();
-
+//1booking created..!
 	@PostMapping
 	public String createBooking(@RequestBody Booking booking){
 		bookings.add(booking);
